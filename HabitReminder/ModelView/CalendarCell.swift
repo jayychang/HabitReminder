1e0f032e6a704fe6435b242608ba5b2ea42747be
Jay Chang <jaychanggg@gmail.com>
2016-10-17 17:17:01 -0700

    Font change test

--- HabitReminder/ModelView/CalendarCell.swift
+++ HabitReminder/ModelView/CalendarCell.swift
@@ -23,45 +23,45 @@ class CalendarCell: JTAppleDayCellView {
 //        background.layer.masksToBounds = true
 //        background.layer.cornerRadius = background.frame.width / 2
         
-        configureTextColor(cellState)
+        configureBackgroundAlpha(cellState)
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy MM dd"
-        let convertedDate: String = formatter.string(from: date)
+        let cellDate: String = formatter.string(from: date)
 
-        print("cell date \(convertedDate)")
+        print("cell date \(cellDate)")
         
         let realm = try! Realm()
         
-        let day = realm.objects(Day.self).filter("date == '\(convertedDate)'")
+        let day = realm.objects(Day.self).filter("date == '\(cellDate)'")
         
         print("\(day.count)")
         
         if day.count > 0 {
             configureCompletion(completion: (day.first?.completed)!)
         } else {
+            dayLabel.font = UIFont(name: "Helvetica-Light", size: 20)
             dayLabel.textColor = UIColor.black
             dayLabel.text =  cellState.text
         }
     }
 
     func configureCompletion ( completion: Bool) {
+        dayLabel.font = UIFont(name: "Zapf Dingbats", size: 40)
         if completion == true {
             dayLabel.textColor = UIColor.green
-            dayLabel.text = "\u{2714}"
+            dayLabel.text = "✔"
         } else {
             dayLabel.textColor = UIColor.red
             dayLabel.text = "✘"
         }
     }
     
-    func configureTextColor(_ cellState: CellState) {
+    func configureBackgroundAlpha(_ cellState: CellState) {
         if cellState.dateBelongsTo == .thisMonth {
             background.alpha = 1
-//            dayLabel.textColor = normalDayColor
         } else {
-//            dayLabel.textColor = weekendDayColor
-            background.alpha = 0.2
+            background.alpha = 0.15
         }
     }
 }
